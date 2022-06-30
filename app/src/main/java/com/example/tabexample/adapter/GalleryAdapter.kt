97af83083bbc0f8c0cdcc548f6bebb0f67bc9f92
chlee973 <chlee973@kaist.ac.kr>
2022-07-01 02:27:12 +0900

    Implement Gallery

---
+++ app/src/main/java/com/example/tabexample/adapter/GalleryAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.tabexample.adapter
+
+import android.content.Context
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tabexample.R
+import com.example.tabexample.model.GalleryImage
+
+class GalleryAdapter(
+    private val context: Context,
+    private val dataset: List<GalleryImage>
+): RecyclerView.Adapter<GalleryAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+        val imageView: ImageView = view.findViewById(R.id.gallery_image)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.gallery_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.imageView.setImageResource(item.imageResourceId)
+    }
+
+    override fun getItemCount() = dataset.size
+}
---
+++ app/src/main/java/com/example/tabexample/model/GalleryImage.kt
@@ -0,0 +1,7 @@
+package com.example.tabexample.model
+
+import androidx.annotation.DrawableRes
+
+data class GalleryImage(
+    @DrawableRes val imageResourceId: Int
+)
