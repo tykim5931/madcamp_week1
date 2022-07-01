9ec24019997fb14fdeb40b0b2cb5eb4fe9d1a16b
chlee973 <chlee973@kaist.ac.kr>
2022-07-01 16:32:08 +0900

    Add floating action button

--- app/src/main/java/com/example/tabexample/Fragment02.kt
+++ app/src/main/java/com/example/tabexample/Fragment02.kt
@@ -1,9 +1,13 @@
 package com.example.tabexample
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.tabexample.adapter.GalleryAdapter
@@ -14,6 +18,14 @@ class Fragment02 : Fragment() {
     private var _binding: FragmentGalleryBinding? = null
     private val binding get() = _binding!!
     private val galleryDataset = GalleryDatasource().loadGallery()
+
+    companion object{
+        const val REQUEST_IMAGE_GET = 1
+    }
+
+    val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        // Handle the returned Uri
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +40,12 @@ class Fragment02 : Fragment() {
         val recyclerView = binding.recyclerView
         recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
         recyclerView.adapter = GalleryAdapter(requireContext(), galleryDataset)
+
+        val fab = binding.fab
+        fab.setOnClickListener {
+            getContent.launch("image/*")
+        }
+
     }
 
     override fun onDestroyView() {
